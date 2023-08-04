960454ed79f12ad2542f78866b76823b0d2a6f3e
ameencaslam <120704619+ameencaslam@users.noreply.github.com>
2023-08-04 08:10:57 +0530

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,21 @@
+// largest among n numbers
+#include <stdio.h>
+void main()
+{
+    int num,i,big;
+    
+    printf("Enter the Numbers. Enter '0' to stop the inupt:-\n\n");
+    
+    do
+    {
+        scanf("%d",&num);
+        
+        if(num>big)
+        {
+            big=num;
+        }
+        
+    } while (num!=0);
+    
+    printf("\n\nLargest Number is: %d",big);
+}
